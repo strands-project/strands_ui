9bc86ada8f7992838813b06bb2a429c7e863a527
Christian Dondrup <cdondrup@lincoln.ac.uk>
2013-10-28 14:14:41 +0000

    Test to see if it works on the robot

--- scripts/marybridge.py
+++ scripts/marybridge.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import httplib, urllib
 
 import StringIO
@@ -20,6 +20,8 @@ import actionlib
 import ros_mary_tts.msg
 import os
 
+from scitos_msgs.msg import HeadLightState
+
 
 speakQueue = Queue()
 replyQueue=Queue()
@@ -38,7 +40,7 @@ class RosMary(object):
         print "Voices: "
         for i in self._voices:
             print " - ",i
-        
+
     def speak(self,req):
         """ Speak service handler """
         speakQueue.put(req.text)
@@ -95,25 +97,25 @@ class maryclient:
         self.port
 
     def set_input_type(self, t):
-        """Set the type of input being 
+        """Set the type of input being
            supplied to the TTS server
            (such as 'TEXT')."""
         self.input_type = t
 
     def get_input_type(self):
-        """Get the type of input being 
+        """Get the type of input being
            supplied to the TTS server
            (such as 'TEXT')."""
         self.input_type
 
     def set_output_type(self, t):
-        """Set the type of input being 
+        """Set the type of input being
            supplied to the TTS server
            (such as 'AUDIO')."""
         self.output_type = t
 
     def get_output_type(self):
-        """Get the type of input being 
+        """Get the type of input being
            supplied to the TTS server
            (such as "AUDIO")."""
         self.output_type
@@ -166,7 +168,7 @@ class maryclient:
         conn = httplib.HTTPConnection(self.host, self.port)
 
         # conn.set_debuglevel(5)
-        
+
         conn.request("POST", "/process", params, headers)
         logging.warning('Watch out!' + params)  # will print a message to the console
 
@@ -190,42 +192,102 @@ class maryclient:
 class pulseplayer:
         def __init__(self, name):
                 self.name = name
+                self.light_pub = rospy.Publisher('/head/cmd_light_state', HeadLightState)
+
+
+        def LEDson(self):
+            lights = HeadLightState()
+            lights.HeadLightInterval = 100
+            lights.LEDState0 = 1
+            lights.LEDPhase0 = 1
+            lights.LEDAmplitude0 = 1.0
+            lights.LEDState1 = 1
+            lights.LEDPhase1 = 1
+            lights.LEDAmplitude1 = 1.0
+            lights.LEDState2 = 1
+            lights.LEDPhase2 = 1
+            lights.LEDAmplitude2 = 1.0
+            lights.LEDState3 = 1
+            lights.LEDPhase3 = 1
+            lights.LEDAmplitude3 = 1.0
+            lights.LEDState4 = 1
+            lights.LEDPhase4 = 1
+            lights.LEDAmplitude4 = 1.0
+            lights.LEDState5 = 1
+            lights.LEDPhase5 = 1
+            lights.LEDAmplitude5 = 1.0
+            lights.LEDState6 = 1
+            lights.LEDPhase6 = 1
+            lights.LEDAmplitude6 = 1.0
+            lights.LEDState7 = 1
+            lights.LEDPhase7 = 1
+            lights.LEDAmplitude7 = 1.0
+            self.light_pub.publish(lights)
+
+        def LEDsoff(self):
+            lights = HeadLightState()
+            lights.HeadLightInterval = 100
+            lights.LEDState0 = 0
+            lights.LEDPhase0 = 1
+            lights.LEDAmplitude0 = 1.0
+            lights.LEDState1 = 0
+            lights.LEDPhase1 = 1
+            lights.LEDAmplitude1 = 1.0
+            lights.LEDState2 = 0
+            lights.LEDPhase2 = 1
+            lights.LEDAmplitude2 = 1.0
+            lights.LEDState3 = 0
+            lights.LEDPhase3 = 1
+            lights.LEDAmplitude3 = 1.0
+            lights.LEDState4 = 0
+            lights.LEDPhase4 = 1
+            lights.LEDAmplitude4 = 1.0
+            lights.LEDState5 = 0
+            lights.LEDPhase5 = 1
+            lights.LEDAmplitude5 = 1.0
+            lights.LEDState6 = 0
+            lights.LEDPhase6 = 1
+            lights.LEDAmplitude6 = 1.0
+            lights.LEDState7 = 0
+            lights.LEDPhase7 = 1
+            self.light_pub.publish(lights)
 
         def play(self, a_sound):
-
+            print "bla"
+            self.LEDsoff()
             buf = StringIO.StringIO(a_sound)
             #
             # pulseaudio / playback stuff
             #
-    
+
             pa = ctypes.cdll.LoadLibrary('libpulse-simple.so.0')
-     
+
             PA_STREAM_PLAYBACK = 1
             PA_SAMPLE_S16LE = 3
             BUFFSIZE = 1024
-    
+
             class struct_pa_sample_spec(ctypes.Structure):
                 __slots__ = [
                     'format',
                     'rate',
                     'channels',
                 ]
-     
+
             struct_pa_sample_spec._fields_ = [
                 ('format', ctypes.c_int),
                 ('rate', ctypes.c_uint32),
                 ('channels', ctypes.c_uint8),
             ]
             pa_sample_spec = struct_pa_sample_spec  # /usr/include/pulse/sample.h:174
-                    
+
             wf = wave.open(buf, 'rb')
             ss = struct_pa_sample_spec()
             ss.rate = wf.getframerate()
             ss.channels = wf.getnchannels()
             ss.format = PA_SAMPLE_S16LE
             error = ctypes.c_int(0)
-    
-    
+
+
             s = pa.pa_simple_new(
                 None,  # Default server.
                 'marybridge',  # Application's name.
@@ -240,28 +302,31 @@ class pulseplayer:
             if not s:
                 raise Exception('Could not create pulse audio stream: {0}!'.format(
                     pa.strerror(ctypes.byref(error))))
-    
+
             while True:
                 # latency = pa.pa_simple_get_latency(s, error)
                 # if latency == -1:
                 #    raise Exception('Getting latency failed!')
-            
+
                 # print('{0} usec'.format(latency))
-            
+
                 # Reading frames and writing to the stream.
                 buf = wf.readframes(BUFFSIZE)
+                self.LEDson()
                 if buf == '':
                     break
-            
+
                 if pa.pa_simple_write(s, buf, len(buf), error):
                     raise Exception('Could not play file!')
-            
+                self.LEDsoff()
+
+            self.LEDsoff()
             wf.close()
-            
+
             # Waiting for all sent data to finish playing.
             if pa.pa_simple_drain(s, error):
                 raise Exception('Could not simple drain!')
-            
+
             # Freeing resources and closing connection.
             pa.pa_simple_free(s)
 
@@ -273,7 +338,7 @@ player = pulseplayer("Mary")
 
 if __name__ == "__main__":
     rospy.init_node('mary_tts')
-
+    print "start"
     client = maryclient()
     rosmary = RosMary(client)
     #speak_server(client)
@@ -288,27 +353,27 @@ if __name__ == "__main__":
     # client.set_voice ("dfki-obadiah-hsmm")
     # client.set_voice ("cmu-bdl-hsmm")
     # client.set_voice ("cmu-rms-hsmm")
-    
+
     # english, female
     # client.set_voice ("dfki-poppy")
     # client.set_voice ("dfki-poppy-hsmm")
     # client.set_voice ("dfki-prudence")
     client.set_voice ("dfki-prudence-hsmm")
     # client.set_voice ("cmu-slt-hsmm")
-    
+
     # german, male
     # client.set_voice ("dfki-pavoque-neutral")
     # client.set_voice ("dfki-pavoque-neutral-hsmm")
     # client.set_voice ("dfki-pavoque-styles")
     # client.set_voice ("bits3")
     # client.set_voice ("bits3-hsmm")
-    
+
     # german, female
     # client.set_voice ("bits1-hsmm")
-    
+
     # telugu, female
     # client.set_voice ("cmu-nk-hsmm")
-    
+
     # turkish, male
     # client.set_voice ("dfki-ot-hsmm")
 
