c0cc8c94b9596010a76e31d2d802dd8c9bd489bd
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2013-11-08 15:29:29 +0000

    Working test script for rendering a page

--- strands_webserver/scripts/test_client.py
+++ strands_webserver/scripts/test_client.py
@@ -14,22 +14,41 @@ import rospy
 from strands_webserver.srv import *
 from std_msgs.msg import String
 
+""" switch the webserver to server pages from dir """
+def set_http_root(dir):
+	rospy.wait_for_service('/strands_webserver/set_http_root')
+ 	set_http_root = rospy.ServiceProxy('/strands_webserver/set_http_root', SetRoot)
+	try:
+  		resp = set_http_root(dir)
+	except rospy.ServiceException as exc:
+  		print("Service did not process request: " + str(exc))
+              
+
+
+""" tell server to display the page relative to server root """
+def display_relative_page(displayNo, page):
+	rospy.wait_for_service('/strands_webserver/display_page')
+ 	display_page = rospy.ServiceProxy('/strands_webserver/display_page', SetDisplay)
+	try:
+  		resp = display_page(displayNo, 'http://localhost:8090/' + page)
+	except rospy.ServiceException as exc:
+  		print("Service did not process request: " + str(exc))
 
 
 if __name__ == '__main__':
 	rospy.init_node("test_client")
 
 	# display some content
-	displayNo = 2
+	displayNo = rospy.get_param("~display", 1)	
 
-	# send some content to appear in the display
+	rospy.loginfo('writing to display: %s', displayNo)
 
- 	rospy.wait_for_service('/strands_webserver/display_page')
- 	display_page = rospy.ServiceProxy('/strands_webserver/display_page', SetDisplay)
-	try:
-  		resp = display_page(displayNo, 'http://www.cs.bham.ac.uk')
-	except rospy.ServiceException as exc:
-  		print("Service did not process request: " + str(exc))
+	# switch the server to display relative to 
+	set_http_root(roslib.packages.get_pkg_dir('marathon_touch_gui'))
+	# and ask it show a page
+	display_relative_page(displayNo, 'index.html')
+
+ 	
               
 
 
