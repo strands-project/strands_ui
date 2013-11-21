f9f07ee80ab4b750cad1fedcd30f8ca0e7b305bb
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2013-11-21 22:01:59 +0000

    Changes to GUI javascript code due to changes in JSON structure returned from log utils.

--- marathon_touch_gui/js/marathon.js
+++ marathon_touch_gui/js/marathon.js
@@ -57,17 +57,40 @@ Fills the node which matches the jquery selector with a summary of the stats of 
 
 function writeLatestEpisodeSummary(ros, selector) {
   
+
+
     getLatestEpisodeSummary(ros, function(summary) {
+
+      summary = summary[0]
 
         // document.getElementById(id).innerHTML = JSON.stringify(summary);
 
-// 
+        var days = Math.floor(summary.run_duration / 86400);
+        var hours = Math.floor(summary.run_duration / 3600) % 24;
+        var minutes = Math.floor(summary.run_duration / 60) % 60;
+
 
 // {"distance":0,"failed_waypoints":0,"bump_recoveries":0,"successful_waypoints":0,"episode_name":"1384875846.717848062","charge_cycles":0,"active_waypoint":"charging_point","date":"2013-11-19 15:44:06.717880","run_duration":"0:00:01.006879","navigation_recoveries":0}
+        if(minutes < 10) 
+          minutes = '0' + minutes;
+
+        if(hours < 10)
+          hours = '0' + hours
+
+        var duration = hours + ':' +  minutes;
+
+        if(days > 0)
+          duration = days + ' days ' + duration;
+
+
+        // console.log(days)
+        // console.log(hours)
+        // console.log(minutes)
+        // console.log(duration)
 
 
         var view = {
-            run_duration: summary.run_duration.substring(0,summary.run_duration.indexOf(".")),
+            run_duration: duration,
             distance: summary.distance,
             bump_recoveries: summary.bump_recoveries,
             charge_cycles: summary.charge_cycles,
@@ -78,14 +101,15 @@ function writeLatestEpisodeSummary(ros, selector) {
           };
 
 
-        var table = '<table class="table"> <tr> <td>Run time</td><td>{{run_duration}} <em>hours</em></td></tr><tr><td>Distance Covered</td> <td>{{distance}} <em>m</em></td></tr> <tr><td>Current Target</td> <td>{{active_waypoint}}</td></tr> <tr><td>Waypoints Reached</td> <td>{{waypoint_successess}}/{{waypoint_attempts}}</td></tr> <tr><td>Bump Recoveries</td> <td>{{bump_recoveries}}</td></tr> <tr><td>Navigation Recoveries</td> <td>{{navigation_recoveries}}</td></tr> <tr><td>Charge Cycles</td> <td>{{charge_cycles}}</td></tr> </table>';
+        var table = '<table class="table"> <tr> <td>Run time</td><td>{{run_duration}} hours</td></tr><tr><td>Distance Covered</td> <td>{{distance}} m</td></tr> <tr><td>Current Target</td> <td>{{active_waypoint}}</td></tr> <tr><td>Waypoints Reached</td> <td>{{waypoint_successess}}/{{waypoint_attempts}}</td></tr> <tr><td>Bump Recoveries</td> <td>{{bump_recoveries}}</td></tr> <tr><td>Navigation Recoveries</td> <td>{{navigation_recoveries}}</td></tr> <tr><td>Charge Cycles</td> <td>{{charge_cycles}}</td></tr> </table>';
         
         var output = Mustache.render(table, view);
      
         //select the template and transform it
         $(selector).html(output)
 
-        console.dir(JSON.stringify(summary));
+         // console.dir(JSON.stringify(summary));
+
     });
 
 
