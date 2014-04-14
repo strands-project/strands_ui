1d62f16697724130a6087d6af5516a58d3a7f867
Christian Dondrup <cdondrup@lincoln.ac.uk>
2014-04-14 16:12:46 +0100

    Using a German male voice for speech synthesis.
    See https://github.com/strands-project/memory_game/issues/47

--- scripts/marybridge.py
+++ scripts/marybridge.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import httplib, urllib
 
 import StringIO
@@ -38,7 +38,7 @@ class RosMary(object):
         print "Voices: "
         for i in self._voices:
             print " - ",i
-        
+
     def speak(self,req):
         """ Speak service handler """
         speakQueue.put(req.text)
@@ -95,25 +95,25 @@ class maryclient:
         self.port
 
     def set_input_type(self, t):
-        """Set the type of input being 
+        """Set the type of input being
            supplied to the TTS server
            (such as 'TEXT')."""
         self.input_type = t
 
     def get_input_type(self):
-        """Get the type of input being 
+        """Get the type of input being
            supplied to the TTS server
            (such as 'TEXT')."""
         self.input_type
 
     def set_output_type(self, t):
-        """Set the type of input being 
+        """Set the type of input being
            supplied to the TTS server
            (such as 'AUDIO')."""
         self.output_type = t
 
     def get_output_type(self):
-        """Get the type of input being 
+        """Get the type of input being
            supplied to the TTS server
            (such as "AUDIO")."""
         self.output_type
@@ -166,7 +166,7 @@ class maryclient:
         conn = httplib.HTTPConnection(self.host, self.port)
 
         # conn.set_debuglevel(5)
-        
+
         conn.request("POST", "/process", params, headers)
         logging.warning('Watch out!' + params)  # will print a message to the console
 
@@ -197,35 +197,35 @@ class pulseplayer:
             #
             # pulseaudio / playback stuff
             #
-    
+
             pa = ctypes.cdll.LoadLibrary('libpulse-simple.so.0')
-     
+
             PA_STREAM_PLAYBACK = 1
             PA_SAMPLE_S16LE = 3
             BUFFSIZE = 1024
-    
+
             class struct_pa_sample_spec(ctypes.Structure):
                 __slots__ = [
                     'format',
                     'rate',
                     'channels',
                 ]
-     
+
             struct_pa_sample_spec._fields_ = [
                 ('format', ctypes.c_int),
                 ('rate', ctypes.c_uint32),
                 ('channels', ctypes.c_uint8),
             ]
             pa_sample_spec = struct_pa_sample_spec  # /usr/include/pulse/sample.h:174
-                    
+
             wf = wave.open(buf, 'rb')
             ss = struct_pa_sample_spec()
             ss.rate = wf.getframerate()
             ss.channels = wf.getnchannels()
             ss.format = PA_SAMPLE_S16LE
             error = ctypes.c_int(0)
-    
-    
+
+
             s = pa.pa_simple_new(
                 None,  # Default server.
                 'marybridge',  # Application's name.
@@ -240,28 +240,28 @@ class pulseplayer:
             if not s:
                 raise Exception('Could not create pulse audio stream: {0}!'.format(
                     pa.strerror(ctypes.byref(error))))
-    
+
             while True:
                 # latency = pa.pa_simple_get_latency(s, error)
                 # if latency == -1:
                 #    raise Exception('Getting latency failed!')
-            
+
                 # print('{0} usec'.format(latency))
-            
+
                 # Reading frames and writing to the stream.
                 buf = wf.readframes(BUFFSIZE)
                 if buf == '':
                     break
-            
+
                 if pa.pa_simple_write(s, buf, len(buf), error):
                     raise Exception('Could not play file!')
-            
+
             wf.close()
-            
+
             # Waiting for all sent data to finish playing.
             if pa.pa_simple_drain(s, error):
                 raise Exception('Could not simple drain!')
-            
+
             # Freeing resources and closing connection.
             pa.pa_simple_free(s)
 
@@ -277,9 +277,9 @@ if __name__ == "__main__":
     client = maryclient()
     rosmary = RosMary(client)
     #speak_server(client)
-    client.set_locale ("en_US")
-    # client.set_locale ("de")
-    client.set_voice ("cmu-slt-hsmm")
+    #client.set_locale ("en_US")
+    client.set_locale ("de")
+    #client.set_voice ("cmu-slt-hsmm")
     # client.set_voice ("dfki-obadiah-hsmm")
 
     # english, male
@@ -288,27 +288,27 @@ if __name__ == "__main__":
     # client.set_voice ("dfki-obadiah-hsmm")
     # client.set_voice ("cmu-bdl-hsmm")
     # client.set_voice ("cmu-rms-hsmm")
-    
+
     # english, female
     # client.set_voice ("dfki-poppy")
     # client.set_voice ("dfki-poppy-hsmm")
     # client.set_voice ("dfki-prudence")
-    client.set_voice ("dfki-prudence-hsmm")
+    #client.set_voice ("dfki-prudence-hsmm")
     # client.set_voice ("cmu-slt-hsmm")
-    
+
     # german, male
     # client.set_voice ("dfki-pavoque-neutral")
-    # client.set_voice ("dfki-pavoque-neutral-hsmm")
+    client.set_voice ("dfki-pavoque-neutral-hsmm")
     # client.set_voice ("dfki-pavoque-styles")
     # client.set_voice ("bits3")
     # client.set_voice ("bits3-hsmm")
-    
+
     # german, female
     # client.set_voice ("bits1-hsmm")
-    
+
     # telugu, female
     # client.set_voice ("cmu-nk-hsmm")
-    
+
     # turkish, male
     # client.set_voice ("dfki-ot-hsmm")
 
