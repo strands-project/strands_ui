9de7be41c151e4666f8a0c51e10e95e43b2e5327
Nils Bore <nilsbore@gmail.com>
2015-02-11 11:16:23 +0100

    Added the media player service

---
+++ src/aaf_music_player/__init__.py
@@ -0,0 +1,3 @@
+import aaf_music_player_server
+
+__all__ = ['aaf_music_player_server']
---
+++ src/aaf_music_player/aaf_music_player_server.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import rospy
+from actionlib_msgs.msg import *
+import actionlib
+from aaf_music_player.srv import *
+
+from os import listdir
+from os.path import isfile, join, splitext
+
+import pygame
+import roslib  
+
+class AAFMusicPlayerServer(object):
+    def __init__(self):
+        rospy.init_node('aaf_music_player_server')
+        folder = rospy.get_param('~folder', '')
+        if len(folder) == 0:
+            rospy.logwarn('No media folder provided!')
+            return
+
+        self.audio_priority = 0.5
+        self.media_path = folder
+        print self.media_path
+        all_files = [ join(self.media_path, f) for f in listdir(self.media_path) if isfile(join(self.media_path, f))]
+        self.files = [ name + ext for (name, ext) in map(lambda f: splitext(f), all_files) if ext == '.mp3' ]
+        self.song_index = 0
+        pygame.mixer.init()
+        self.paused = True
+        self.service = rospy.Service('aaf_music_player_service', MusicPlayerService, self.pressed_button)
+        while not rospy.is_shutdown():
+            if not self.paused and not pygame.mixer.music.get_busy():
+                self.song_index = self.song_index + 1
+                if self.song_index >= len(self.files):
+                    self.song_index = 0
+                pygame.mixer.music.load(self.files[self.song_index])
+                pygame.mixer.music.play()
+            rospy.sleep(1)
+
+    def pressed_button(self, req):
+        if req.player_action == MusicPlayerServiceRequest.PLAY:
+            pygame.mixer.music.load(self.files[self.song_index])
+            pygame.mixer.music.play()
+            self.paused = False
+        elif req.player_action == MusicPlayerServiceRequest.PAUSE:
+            self.paused = True
+            pygame.mixer.music.stop()
+        elif req.player_action == MusicPlayerServiceRequest.PREVIOUS:
+            pygame.mixer.music.stop()
+            self.song_index = self.song_index - 1
+            if self.song_index < 0:
+                self.song_index = len(self.files)-1
+            pygame.mixer.music.load(self.files[self.song_index])
+            pygame.mixer.music.play()
+            self.paused = False
+        elif req.player_action == MusicPlayerServiceRequest.NEXT:
+            print self.files[self.song_index]
+            pygame.mixer.music.stop()
+            self.song_index = self.song_index + 1
+            if self.song_index >= len(self.files):
+                self.song_index = 0
+            print self.files[self.song_index]
+            pygame.mixer.music.load(self.files[self.song_index])
+            pygame.mixer.music.play()
+            self.paused = False
+        else:
+            rospy.logwarn('Service argument has to be 0-3: PLAY, PAUSE, NEXT, PREVIOUS')
+
+        return MusicPlayerServiceResponse(self.files[self.song_index], self.audio_priority)
+        
+
+if __name__ == '__main__':
+    server = AAFMusicPlayerServer()
+
