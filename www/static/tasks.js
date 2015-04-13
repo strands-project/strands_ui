9a9b9d85bdd3bcd2ae43ce94150d55992cccd806
Marc Hanheide (at AAF control PC) <marc@hanheide.net>
2015-04-13 17:34:34 +0200

    now displays times correctly

--- www/static/tasks.js
+++ www/static/tasks.js
@@ -25,12 +25,13 @@
         
         html = ""
         for (var t=0; t < message.execution_queue.length; t++) {
-            task = message.execution_queue[t];
+            var task = message.execution_queue[t];
+            var date = new Date(task.execution_time.secs*1000).toLocaleString('de-AT', { timeZone: 'Europe/Vienna' });
             html += "<tr data-toggle=\"modal\" data-target=\"#deletetask\" data-whatever=\" * task.task_id + \">";
             html += "<td>" + task.task_id + "</td>";
             html += "<td>" + task.task_id + "</td>";
             html += "<td>" + task.start_node_id + "</td>";
-            html += "<td>" + "time"+ "</td>";
+            html += "<td>" + date + "</td>";
             html += "</tr>";
            
         }
